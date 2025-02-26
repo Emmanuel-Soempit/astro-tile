5544fcb9ad26a4da8a0786d442519490b6663311
Legend MacBook Pro <ayomiposi@Legendss-MacBook-Pro.local>
2025-02-26 20:01:43 +0100

    Updates

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        card: "var(--card-background)",
+      },
+    },
+  },
+  plugins: [],
+};
